eabf28616454686c85e553131bdb617a0df59347
deepak01111 <kushwahadeepak278@gmail.com>
2023-11-17 16:51:38 +0530

    changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
 
       export const io = new Server(server,{
             cors:{
-                origin:"http://localhost:5173",
+                origin:`${process.env.FRONTEND}`,
             }
         });
 
